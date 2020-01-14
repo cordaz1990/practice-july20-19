23be2c987cec452a75aa0be7ce3dc9a5f24e4765
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-13 20:52:23 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,23 @@
-print('hello')
-print('word')
-print
+prefixes = 'JKLMNOPQ'
+suffix = 'ack'
+
+for letter in prefixes:
+    print(letter + suffix)
+    
+    
+>>> Jack
+>>> Kack
+>>> Lack
+>>> Mack
+>>> Nack
+>>> Oack
+>>> Pack
+>>> Qack
+
+#String Slicing 
+>>> s = 'Monty Python'
+>>> s[0:5]
+'monty'
+>>>s[6:12]
+'python'
+
